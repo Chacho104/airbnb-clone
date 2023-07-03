c0a89498bb3705257922c5d02da297cde4eee5d2
Churchil Owino <chachowino75@gmail.com>
2023-07-03 15:58:49 +0300

    Create the logo component

---
+++ src/app/components/navbar/Logo.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const Logo: React.FC = () => {
+  const router = useRouter;
+  return (
+    <Image
+      alt="Logo"
+      height={100}
+      width={100}
+      src="/images/logo.png"
+      className="hidden md:block cursor-pointer"
+    />
+  );
+};
+export default Logo;

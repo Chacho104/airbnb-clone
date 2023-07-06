2e6ff39e2157822450890de05db96bb4a0e71034
Churchil Owino <chachowino75@gmail.com>
2023-07-06 15:15:33 +0300

    Remove unnecessary gap classname

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-"use-client";
+"use client";
 import React from "react";
 import Container from "../Container";
 import Logo from "./Logo";
@@ -12,7 +12,7 @@ const Navbar: React.FC<NavbarProps> = () => {
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
         <Container>
-          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+          <div className="flex flex-row items-center justify-between">
             <Logo />
             <Search />
             <UserMenu />

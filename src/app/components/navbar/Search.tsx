d31919e72877a502df1dfe72e79042744b15d808
Churchil Owino <chachowino75@gmail.com>
2023-07-18 20:18:49 +0300

    Automatically sort classnames with prettier

--- src/app/components/navbar/Search.tsx
+++ src/app/components/navbar/Search.tsx
@@ -5,19 +5,19 @@ import { BiSearch } from "react-icons/bi";
 
 const Search: React.FC = () => {
   return (
-    <div className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
+    <div className="w-full cursor-pointer rounded-full border-[1px] py-2 shadow-sm transition hover:shadow-md md:w-auto">
       <div className="flex flex-row items-center justify-between">
-        <div className="text-sm font-semibold px-6 md:w-24 lg:w-auto truncate">
+        <div className="truncate px-6 text-sm font-semibold md:w-24 lg:w-auto">
           Anywhere
         </div>
-        <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center md:w-24 lg:w-auto truncate">
+        <div className="hidden flex-1 truncate border-x-[1px] px-6 text-center text-sm font-semibold sm:block md:w-24 lg:w-auto">
           Any week
         </div>
-        <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
-          <div className="hidden sm:block md:w-12 lg:w-auto truncate">
+        <div className="flex flex-row items-center gap-3 pl-6 pr-2 text-sm text-gray-600">
+          <div className="hidden truncate sm:block md:w-12 lg:w-auto">
             Add guests
           </div>
-          <div className="p-2 bg-rose-500 rounded-full text-white">
+          <div className="rounded-full bg-rose-500 p-2 text-white">
             <BiSearch size={18} />
           </div>
         </div>

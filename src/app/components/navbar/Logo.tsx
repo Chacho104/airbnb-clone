5ad6f2fe5fd0c9d9849517a2b6f0d7d8aa03f294
Churchil Owino <chachowino75@gmail.com>
2023-07-18 20:19:32 +0300

    Sort classnames with prettier

--- src/app/components/navbar/Logo.tsx
+++ src/app/components/navbar/Logo.tsx
@@ -11,7 +11,7 @@ const Logo: React.FC = () => {
       height={100}
       width={100}
       src="/images/logo.png"
-      className="hidden md:block cursor-pointer"
+      className="hidden cursor-pointer md:block"
     />
   );
 };
